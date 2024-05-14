bfe8c21e8e4cbfda27610efb66ff7c80ebcdc5c8
APIVerve Team <hello@apiverve.com>
2024-05-14 15:25:22 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,39 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class owner
+{
+    [JsonProperty("registrar")]
+    public string registrar { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("domain")]
+    public string domain { get; set; }
+
+    [JsonProperty("isAvailable")]
+    public bool isAvailable { get; set; }
+
+    [JsonProperty("owner")]
+    public owner owner { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
